1d1649e89bde9c94e3309eb3e573c310411554bc
Nitin Kumar <nitinkumardhiman2@gmail.com>
2022-08-13 00:59:42 +0530

    generating Subsequence Code

---
+++ Heap/generatingSubsequence.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Approach 1: POWER SET 
+//Time Complexity : O(N.2^N)
+//Space Complexity : O(1) ignoring the auxillary 2d array as it is return 
+vector<vector<int>>generateSubsequenceUsingPowerSet(vector<int>&nums){
+    //Defining the 2d array size to the 2^n
+    vector<vector<int>>ans(pow(2,nums.size()));
+    for(int i =0 ;i<(1<<nums.size());i++){
+        for(int j = 0;j<nums.size();j++){
+            //Only set bit positioned indexed element in array is pushed to ans array
+            if(i & 1<<j){
+                ans[i].push_back(nums[j]);
+            }
+        }
+    }
+    return ans;
+}
+
+//Approach 2: Recursive approach
+//Time Complexity: O()
+//Space Complexity: O()
+
+void generateSubsequenceUsingRecursion(vector<int>&nums, int idx, vector<int>temp, int &index, vector<vector<int>>&ans){
+    if(idx>=nums.size()){
+        ans[index] = temp;
+        index++;
+        return;
+    }
+    temp.push_back(nums[idx]);
+    generateSubsequenceUsingRecursion(nums,idx+1,temp,index,ans);
+    temp.pop_back();
+    generateSubsequenceUsingRecursion(nums,idx+1,temp,index,ans);
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>nums(n);
+    for(auto &i:nums){
+        cin>>i;
+    }
+    vector<vector<int>>ans(pow(2,nums.size()));
+    vector<int>temp;
+    int index = 0;
+    generateSubsequenceUsingRecursion(nums,0,temp,index,ans);
+    for(auto i:ans){
+        for(auto j:i){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
