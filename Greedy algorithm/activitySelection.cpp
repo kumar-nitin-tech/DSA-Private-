fb753fc56af7e1db9c9d8d688b46dddf9db0196c
Shyam Sundar Patro <lionelsam09@gmail.com>
2023-01-21 18:58:31 +0530

    change

--- Greedy algorithm/activitySelection.cpp
+++ Greedy algorithm/activitySelection.cpp
@@ -1,8 +1,3 @@
-/*
-    There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish 
-    time of meeting i. What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room
-    at a particular time?
-*/
 
 #include<bits/stdc++.h>
 using namespace std;
